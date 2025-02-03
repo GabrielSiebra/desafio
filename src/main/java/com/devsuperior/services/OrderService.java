037fcb80d1b87ea220f737cdcd9d075241104516
GabrielSiebra <gabrielsiebra2014@gmail.com>
2025-02-03 20:37:56 -0300

    OrderService

--- src/main/java/com/devsuperior/services/OrderService.java
+++ src/main/java/com/devsuperior/services/OrderService.java
@@ -1,8 +1,21 @@
 package com.devsuperior.services;
 
+import com.devsuperior.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class OrderService {
 
-    public
+    @Autowired
+    private ShippingService shippingService;
+
+    public double total(Employee order) {
+
+        Double ValueDiscount = order.getBasic() * (1 - order.getDiscount() / 100);
+        order.setBasic(ValueDiscount);
+        return order.getBasic() + shippingService.shipment(order);
+
+    }
+
 
 
     }
